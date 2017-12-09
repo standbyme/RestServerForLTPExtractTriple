d3843ea87482d862da49a399d6c42c112b710fd5
tgs <ddd@ss.gg>
2017-12-09 16:18:12 +0800

    Add reqparse

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-
 class HelloWorld(Resource):
     def post(self):
-        return {'hello': 'world'}
+        parser = reqparse.RequestParser()
+        parser.add_argument('idd', type=int, help='Rate to charge for this resource')
+        args = parser.parse_args()
+        return {'hello': args['idd']+233}
 
 
 api.add_resource(HelloWorld, '/')
