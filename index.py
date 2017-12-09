3526196e94ea90a514eeaeeba1d494f7781ff1e1
tgs <ddd@ss.gg>
2017-12-09 15:58:56 +0800

    Add CORS

--- index.py
+++ index.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
 class HelloWorld(Resource):
-    def get(self):
+    def post(self):
         return {'hello': 'world'}
 
 
