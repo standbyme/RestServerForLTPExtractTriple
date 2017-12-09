70028be93180daf67636ce2dd80de55430b970c9
tgs <ddd@ss.gg>
2017-12-09 18:13:35 +0800

    Add Mock

--- index.py
+++ index.py
@@ -10,12 +10,22 @@ app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
+
 class HelloWorld(Resource):
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('UserInput', type=str, help='UserInput')
         args = parser.parse_args()
-        return {'Result': urllib.unquote(args['UserInput']).split('\n')}
+        result = [{"HeadEntity": "小绵羊",
+                   "Relation": "吃",
+                   "TailEntity": "草",
+                   "SentenceStructure": "益智游戏"},
+                  {"HeadEntity": "小绵羊小绵羊小绵羊小绵羊小绵羊小绵羊小绵羊小绵羊",
+                   "Relation": "吃",
+                   "TailEntity": "草",
+                   "SentenceStructure": "益智游戏"}]
+        # {'Result': urllib.unquote(args['UserInput']).split('\n')}
+        return {'Result': result}
 
 
 api.add_resource(HelloWorld, '/')
