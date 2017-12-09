b9489e97fc598bfe8a16efb0b73e3e313e76f372
tgs <ddd@ss.gg>
2017-12-09 16:56:57 +0800

    Resolve unicode problem

--- index.py
+++ index.py
@@ -1,6 +1,10 @@
+# coding:utf-8
+
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_cors import CORS
+
+import urllib
 
 app = Flask(__name__)
 CORS(app)
@@ -9,9 +13,9 @@ api = Api(app)
 class HelloWorld(Resource):
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('idd', type=int, help='Rate to charge for this resource')
+        parser.add_argument('idd', type=str, help='Rate to charge for this resource')
         args = parser.parse_args()
-        return {'hello': args['idd']+233}
+        return {'hello': urllib.unquote(args['idd'])}
 
 
 api.add_resource(HelloWorld, '/')
